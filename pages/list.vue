1418f5cfe62ddfd7817d2efe986ab8a1266b6716
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2022-10-04 22:38:35 +0800

    update: list currency with value 1

--- pages/list.vue
+++ pages/list.vue
@@ -1,57 +1,34 @@
 <template>
   <main class="min-h-screen mt-4 bg-base-100">
-    <div
-      class="max-w-xl px-4 mx-auto grid items-center"
-      v-if="current_currency != null"
-    >
-      <div class="border rounded p-4 max-w-xs mx-auto">
+    <div class="max-w-xl px-4 mx-auto grid items-center" v-if="current_currency != null">
+      <div class="border rounded p-4 max-w-sm mx-auto">
         <div class="flex justify-between items-center border rounded mb-4">
           <div class="flex justify-between">
             <div class="flex items-center w-1/3">
-              <select
-                class="select"
-                name=""
-                id=""
-                v-if="country != null"
-                @change="updateCurrentCurrency"
-              >
+              <select class="select" name="" id="" v-if="country != null" @change="updateCurrentCurrency">
                 <optgroup v-for="(c, k) in country" :key="k" :label="k">
-                  <option
-                    v-for="(cc, kk) in c"
-                    :key="kk"
-                    :selected="kk == current_currency"
-                  >
+                  <option v-for="(cc, kk) in c" :key="kk" :selected="kk == current_currency">
                     {{ kk }}
                   </option>
                 </optgroup>
               </select>
               <label v-else class="btn loading"></label>
             </div>
-            <input
-              type="number"
-              id="default"
-              class="input text-right w-1/2"
-              value="1"
-              @keyup="updateValue"
-            />
+            <input type="number" id="default" class="input text-right w-1/2" value="1" @keyup="updateValue" />
           </div>
         </div>
 
         <div class="border-b my-4"></div>
 
         <div v-if="currency.length != 0">
-          <div
-            class="flex justify-between items-center border rounded mb-4"
-            v-for="(x, l) in currency"
-            :key="l"
-            :id="x"
-          >
-            <div class="flex flex-1 justify-between px-2">
+          <div class="flex justify-between items-center border rounded mb-4" v-for="(x, l) in currency" :key="l"
+            :id="x">
+            <div class="flex flex-1 justify-between px-2 border-r">
               <div class="flex items-center">{{ x }}</div>
-              <div></div>
+              <div class="my-4 text-right"></div>
             </div>
-            <div class="border-l delete">
-              <button class="btn" @click="deleteCurrency(x)">X</button>
+            <div class="delete">
+              <button class="btn btn-ghost btn-sm" @click="deleteCurrency(x)">X</button>
             </div>
           </div>
         </div>
@@ -61,28 +38,15 @@
         </div>
 
         <div class="flex justify-between mt-4">
-          <label
-            class="btn btn-sm rounded modal-button"
-            for="show_country"
-            @click="getCountry"
-          >
+          <label class="btn btn-sm rounded modal-button" for="show_country" @click="getCountry">
             + Add Currency
           </label>
           <button class="btn btn-sm rounded" id="refresh" @click="getConvert">
             <!-- refresh icon -->
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
-              />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
             </svg>
           </button>
         </div>
@@ -99,18 +63,9 @@
           <div class="flex justify-between">
             <div class="flex-1">
               <label class="btn btn-sm rounded" for="show_country">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  class="inline-block w-5 h-5 stroke-current"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M15 19l-7-7 7-7"
-                  ></path>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                  class="inline-block w-5 h-5 stroke-current">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
                 </svg>
               </label>
             </div>
@@ -120,37 +75,22 @@
             <div class="flex-1 text-right"></div>
           </div>
           <div class="mt-2">
-            <input
-              type="text"
-              class="input w-full"
-              placeholder="Search"
-              id="search"
-              autocomplete="off"
-              @keyup="search"
-            />
+            <input type="text" class="input w-full" placeholder="Search" id="search" autocomplete="off"
+              @keyup="search" />
           </div>
         </div>
         <div v-if="country != null" class="mt-4">
           <div v-for="(c, k) in country" :key="k" class="border-b mb-2">
             <div class="flex items-center font-bold text-lg">{{ k }}</div>
             <div v-for="(cc, kk) in c" :key="kk" class="p-2 currency_parent">
-              <label
-                v-if="currency.includes(kk)"
-                class="flex justify-between line-through currency opacity-50"
-                :id="kk"
-              >
+              <label v-if="currency.includes(kk)" class="flex justify-between line-through currency opacity-50"
+                :id="kk">
                 <span class="mr-2">{{ kk }}</span>
                 <span>-</span>
                 <span class="flex-1 ml-2">{{ cc }}</span>
               </label>
 
-              <label
-                for="show_country"
-                class="flex justify-between currency"
-                :id="kk"
-                @click="addCurrency(kk)"
-                v-else
-              >
+              <label for="show_country" class="flex justify-between currency" :id="kk" @click="addCurrency(kk)" v-else>
                 <span class="mr-2">{{ kk }}</span>
                 <span>-</span>
                 <span class="flex-1 ml-2">{{ cc }}</span>
@@ -328,11 +268,11 @@ export default {
         };
         fetch(
           "https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from=" +
-            this.current_currency +
-            "&to=" +
-            e +
-            "&amount=" +
-            def.value,
+          this.current_currency +
+          "&to=" +
+          e +
+          "&amount=" +
+          def.value,
           requestOptions
         )
           .then((response) => {
@@ -340,9 +280,15 @@ export default {
             return response.json();
           })
           .then((data) => {
-            console.log(data);
-            document.getElementById(e).children[0].children[1].innerHTML =
-              data.result;
+
+            var ex_result = data.result;
+
+            if (def.value != 1) {
+              document.getElementById(e).children[0].children[1].innerHTML = ex_result.toFixed(2) + "<br/ > <span class='text-2xs text-zinc-500'>1 " + this.current_currency + " = " + (ex_result / def.value).toFixed(4) + " " + e + "</span>";
+            } else {
+              document.getElementById(e).children[0].children[1].innerHTML =
+                def.value == 1 ? ex_result.toFixed(4) : ex_result.toLocaleString("en-US");
+            }
 
             this.loading = false;
           })
