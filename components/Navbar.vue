1962ea6bf43c894cb6de9f56ccad24ca81053059
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2023-01-23 13:40:32 +0800

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -7,7 +7,7 @@
       <h1 class="font-bold">Exchange Currency</h1>
     </div>
     <div class="flex-1 dropdown dropdown-end text-right">
-      <button class="btn btn-square btn-ghost" tabindex="0">
+      <label class="btn btn-square btn-ghost" tabindex="0">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -21,7 +21,7 @@
             d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
           ></path>
         </svg>
-      </button>
+      </label>
       <ul
         tabindex="0"
         class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
